af937f815728b1454288c9588d188366758aa6e6
Alexander Souza <alex_bks@hotmail.com>
2019-05-29 16:41:59 -0300

    Pequenos detalhes

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -1,27 +1,25 @@
-require 'rails_helper'
+require 'rest-client'
+require 'json'
 
- 
-RSpec.describe 'Exchanges', type: :request do
-  describe 'GET #index' do
-    it 'returns http success' do
-      get '/'
-      expect(response).to have_http_status(200)
-    end
+class ExchangeService
+  def initialize(source_currency, target_currency, amount)
+    @source_currency = source_currency
+    @target_currency = target_currency
+    @amount = amount.to_f
   end
 
-
-  describe 'GET #convert' do
-    before do
-      @amount = rand(1..9999)
-    end
  
-    it 'returns http success' do
-      get '/convert', params: {
-                        source_currency: "USD",
-                        target_currency: "BRL",
-                        amount: @amount
-                      }
-      expect(response).to have_http_status(200)
+  def perform
+    begin
+      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
+      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+      res = RestClient.get url
+      value = JSON.parse(res.body)['currency'][0]['value'].to_f
+      
+      value * @amount
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
     end
   end
 end
